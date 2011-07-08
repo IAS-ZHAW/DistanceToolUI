d4fd875effec3720d9772f05aac1272696454061
Niederb <thomas@niederberger.ch>
2011-07-08 11:54:28 +0000

    git-svn-id: https://minnie.zhaw.ch/svn/distanztool/DistanceToolUI@71 faa62320-0647-4c55-9a10-af1b984ca1a2

--- src/ch/zhaw/ias/dito/ui/resource/AppConfig.java
+++ src/ch/zhaw/ias/dito/ui/resource/AppConfig.java
@@ -1,15 +1,22 @@
 package ch.zhaw.ias.dito.ui.resource;
 
 import java.awt.Color;
+import java.util.ResourceBundle;
 
 import javax.swing.UIManager;
 
 public class AppConfig {
+  public static final String BUILD_DATE;
+  public static final String BUILD_NUMBER;
   public static final Color ACTIVE;
   
+  
   static {
-    //Color c = UIManager.getColor("Button.focus");
-    //Color c = UIManager.getColor("textHighlight");
+
+    ResourceBundle versionBundle = ResourceBundle.getBundle("ch.zhaw.ias.dito.ui.resource.version");
+    BUILD_DATE = versionBundle.getString("buildDate");
+    BUILD_NUMBER = versionBundle.getString("buildNum");
+
     Color c = UIManager.getColor("info");
     if (c != null) {
       ACTIVE = c;
